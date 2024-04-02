f2401168e3a4bc4b82ff9aee79f9d093768ad7a9
Mora <dacraxus@gmail.com>
2024-04-02 02:30:33 +0200

    added BoatFactory.php

---
+++ database/factories/BoatFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Boat;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+
+class BoatFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Boat::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->faker->name;
+
+        return [
+            'name' => $name,
+            'category' => $this->faker->randomElement(['sailing-yacht', 'motor-boat']),
+            'slug' => Str::slug($name),
+        ];
+    }
+}
